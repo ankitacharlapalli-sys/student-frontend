8d08b15b53ec5d6fd5830d1d506419bdea811aaf
ankitacharlapalli-sys <ankitacharlapalli@gmail.com>
2026-08-25 18:50:07 +0530

    connect frontend to live backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "http://localhost:8081/api";
+const API = "http://student-backend-fcsi.onrender.com/api";
 
 function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user") || "null"));
